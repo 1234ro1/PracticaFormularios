bd6b06732db7b9a67e5439605d2e6283faf23c50
1234ro1 <josue.ramirez@catolica.edu.sv>
2023-04-28 18:11:23 -0600

    Agregar archivos de proyecto.

---
+++ PracticaFormularios/Controllers/equipos.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PracticaFormularios.Models;
+
+namespace PracticaFormularios.Controllers
+{
+    public class equipos : Controller
+    {
+        private readonly equiContext _equiContext; 
+        public  equipos(equiContext equiContext) 
+        {
+            _equiContext = equiContext;
+        }
+        public IActionResult Index()
+        {
+            var listadoDeMarcas = (from m in _equiContext.marcas select m).ToList();
+            ViewData["listadoDeMarcas"] = new SelectList(listadoDeMarcas, "id_marcas", "nombre_marca");
+            
+
+            var listadoTipoEquipo = (from tp in _equiContext.tipo_equipo select tp).ToList();
+            ViewData["listadoTipoEquipos"] = new SelectList(listadoTipoEquipo, "id_tipo_equipo", "descripcion");
+
+
+            var listadoEstadosEquipo = (from eq in _equiContext.estados_equipo select eq).ToList();
+            ViewData["listadoEstadosEquipo"] = new SelectList(listadoEstadosEquipo, "id_estados_equipo", "estado");
+            return View();
+        }
+        public IActionResult crearRegistro(equipos nuevosEquipo)
+        {
+            _equiContext.Add(nuevosEquipo);
+            _equiContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PracticaFormularios/Models/equiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ultima.Models;
+
+namespace PracticaFormularios.Models
+{
+    public class equiContext : DbContext
+    {
+        public equiContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<equipos> equipos { get; set; }
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<tipo_equipo> tipo_equipo { get; set; }
+        public DbSet<estados_equipo> estados_equipo { get; set; }
+
+
+
+    }
+}
